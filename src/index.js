76e98f638f6cb295b644ce693ec412039b1ed5b0
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-15 22:28:40 +0900

    論理演算子の本当の意味を知ろう && ||

--- src/index.js
+++ src/index.js
@@ -193,3 +193,26 @@
 //   return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
 // };
 // console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2はtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
