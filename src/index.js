7ac21d0d7daadfcdd446550e56b29249893c904e
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-15 19:52:09 +0900

    コメントアウト

--- src/index.js
+++ src/index.js
@@ -67,16 +67,16 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
-console.log(func3(10, 20));
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(10, 20));
