958ad068d5f2c3d6b8e713cc9be366a5195acc8d
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-15 22:19:09 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -175,3 +175,21 @@
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueのとき : 条件がfalseのとき
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = 1300;
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 60));
