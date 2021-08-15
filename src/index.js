4d006ee873746f4d184eb3005e841f949fd3e7b9
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-15 21:41:13 +0900

    デフォルト値、引数など

--- src/index.js
+++ src/index.js
@@ -104,3 +104,9 @@
 // const [name, age] = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}歳です。`;
 // console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+sayHello();
