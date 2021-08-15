18969332057b4747686dcdf6ef36fb4c839ec328
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-15 19:42:59 +0900

    コメントアウト

--- src/index.js
+++ src/index.js
@@ -48,14 +48,14 @@
 /**
  * テンプレート文字列
  */
-const name = "yuki";
-const age = 34;
-// 「私の名前はyukiです。年齢は34歳です。」
+// const name = "yuki";
+// const age = 34;
+// // 「私の名前はyukiです。年齢は34歳です。」
 
-// 従来の方法
-const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
-console.log(message1);
+// // 従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
 
-// テンプレート文字列を用いた方法
-const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
